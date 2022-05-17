657aeabe573ac0adbc18be650c51ed24614d45ee
Daniel Keywan Hollister <daniel.hollister@gmail.com>
2022-05-17 09:13:30 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="font-sans flex flex-col justify-center items-center">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="hello world" />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  corePlugins: {
+    aspectRatio: false
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
+  ]
+}
